4433e2bda3ad5b9a68f69fdaa31db74429ca3282
Ashish Patil <ashishbpatil1@gmail.com>
2017-01-09 21:05:31 +0530

    First push for athleteAdda git demo
    First push for athleteAdda git demo test

---
+++ GitDemoForAthleteAdda/GitDemoForAthleteAdda/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitDemoForAthleteAdda.MvcApplication" Language="C#" %>
---
+++ GitDemoForAthleteAdda/GitDemoForAthleteAdda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDemoForAthleteAdda.Startup))]
+namespace GitDemoForAthleteAdda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
