00a365f7649b81a683db85b94ef6a70b43dd00ca
sudhanshu <sudhanshu2089@gmail.com>
2017-01-10 17:53:04 +0530

    For testing

--- GitDemoForAthleteAdda/GitDemoForAthleteAdda/Controllers/HomeController.cs
+++ GitDemoForAthleteAdda/GitDemoForAthleteAdda/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GitDemoForAthleteAdda.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Athleteadda Description page.";
 
             return View();
         }
